50b74259872b9c9b98a1b9333bc375c8e8aef403
Patricia Eliseu <patyeliseu4@gmail.com>
2025-09-25 09:59:33 -0300

    add tratamento de erro na divisao

--- aula_exceptions/src/aula_exceptions/Divisao.java
+++ aula_exceptions/src/aula_exceptions/Divisao.java
@@ -1,24 +1,40 @@
 package aula_exceptions;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Divisao {
 
 	static Scanner ler = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
-		
+
 		int dividendo = 0;
 		int divisor = 0;
-			
-		System.out.println("Digite o Dividendo: ");
-		dividendo = ler.nextInt();
-				
-		System.out.println("Digite o Divisor: ");
-		divisor = ler.nextInt();
-				
-		divide(dividendo, divisor);
-		
+		boolean loop = true;
+
+		do {
+			try {
+				System.out.println("Digite o Dividendo: ");
+				dividendo = ler.nextInt();
+				System.out.println("Digite o Divisor: ");
+				divisor = ler.nextInt();
+
+				divide(dividendo, divisor);
+
+				loop = false;
+
+			} catch (InputMismatchException e) {
+				System.err.println("Digite valores inteiros! " + e);
+				ler.nextLine();
+
+			} catch (ArithmeticException e) {
+				System.err.println("Não existe divisão por zero! " + e);
+				ler.nextLine();
+
+			}
+		} while (loop);
+
 	}
 
 	public static void divide(int dividendo, int divisor) {
